abf926342b6b7d035214dcbe29a665717b3b778d
Informatica <informatica@alumno.local>
2026-07-22 10:53:28 -0600

    Feat: se agrego metodos para POST y PUT

--- ServidorCRUD/src/routes/clientesRouter.ts
+++ ServidorCRUD/src/routes/clientesRouter.ts
@@ -19,11 +19,11 @@ export async function clienteRouter(req:IncomingMessage, res:ServerResponse):Pro
             return true;
         }
         //Get by id
-        else if(method === "GET" && url.startsWith("/clientes/")){
+        if(method === "GET" && url.startsWith("/clientes/")){
 
             const id = Number(url.split("/")[2]);
             const cliente = await service.buscar(id);
-            if(!cliente === true){
+            if(!cliente){
                 res.writeHead(404);
                 res.end(JSON.stringify({mensaje:"cliente no encontrado"}));
                 return true;
@@ -32,18 +32,66 @@ export async function clienteRouter(req:IncomingMessage, res:ServerResponse):Pro
             res.end(JSON.stringify(cliente));
             return true;
         }
+        //POST
+        if(method === "POST" && url === "/clientes"){
+            let body = "";
+
+            req.on("data",chunk =>{
+                body += chunk;
+            });
+            req.on("end",async ()=>{
+                try {
+                    const cliente = JSON.parse(body);
+                    await service.agregarCliente(cliente);
+                    res.writeHead(201);
+                    res.end(JSON.stringify({mensaje : "Cliente Creado"}));
+                    return true;
+                } catch (error) {
+                    res.writeHead(400);
+                    res.end(JSON.stringify({mensaje:(error as Error).message}));
+                    return true;
+                }
+            });
+            return true;
+        }
+
+        //PUT
+        if(method === "PUT" && url.startsWith("/clientes/")){
+            let body = "";
+            req.on("data",chunk=>{
+                body += chunk;
+            });
+            req.on("end",async ()=>{
+                try {
+                    const cliente = JSON.parse(body);
+                    cliente.id = Number(url.split("/")[2]);
+                    await service.actualizarCliente(cliente);
+                    res.writeHead(200);
+                    res.end(JSON.stringify({mensaje:"Cliente Actualizado"}));
+                    return true;
+                } catch (error) {
+                    res.writeHead(400);
+                    res.end(JSON.stringify({mensaje:(error as Error).message}));
+                    return true;
+                    
+                }
+            });
+            return true;
+        }
+
         //DELETE
-        else if(method ==="DELETE" && url.startsWith("/clientes/")){
+        if(method ==="DELETE" && url.startsWith("/clientes/")){
             const id = Number(url.split("/")[2]);
             const cliente = await service.eliminar(id);
-            if(!cliente === true){
+            if(!cliente){
                 res.writeHead(404);
                 res.end(JSON.stringify({mensaje:"cliente no encontrado"}));
                 return true;
-            }else
-            res.writeHead(200);
-            res.end(JSON.stringify({mensaje:"cliente eliminado"}));
-            return true;
+            }else{
+                res.writeHead(200);
+                res.end(JSON.stringify({mensaje:"cliente eliminado"}));
+                return true;
+            }
         }
 
         res.writeHead(404);
